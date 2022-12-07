90c7f61e9570b19cc2d8743577784c82500506e6
Caio F Melo <caio.melo@ccc.ufcg.edu.br>
2022-12-06 22:46:41 -0300

    Fix history styles

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -32,28 +32,29 @@ export const HistoryList = styled.main`
 
       &:first-child {
         border-top-left-radius: 8px;
-        padding-left:1.5rem;
+        padding-left: 1.5rem;
       }
-      
+
       &:last-child {
         border-top-right-radius: 8px;
-        padding-right:1.5rem;
+        padding-right: 1.5rem;
+      }
     }
-  }
 
-  td {
-    background-color: ${(props) => props.theme['gray-700']};
-    border-top: 4px solid ${(props) => props.theme['gray-800']};
-    padding:1rem;
-    font-size: 0.875rem;
-    line-height: 1.6;
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
 
-    &:first-child {
+      &:first-child {
         width: 50%;
-        padding-left:1.5rem;
-    }
-    &:last-child {
-        padding-right:1.5rem;
+        padding-left: 1.5rem;
+      }
+      &:last-child {
+        padding-right: 1.5rem;
+      }
     }
   }
 `
